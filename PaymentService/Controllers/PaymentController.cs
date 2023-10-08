60837fac8f70cf04edbb0c1cbcdbb699ad1a031c
kazys <kazys.rackauskas@gmail.com>
2023-10-08 17:02:00 +0300

    empty PaymentController

---
+++ PaymentService/Controllers/PaymentController.cs
@@ -0,0 +1,16 @@
+﻿using EasyTdd.Blog.No1.MoreComplexExample.PaymentService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyTdd.Blog.No1.MoreComplexExample.PaymentService.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PaymentController : ControllerBase
+	{
+		[HttpPost("callback")]
+		public IActionResult Callback(PaymentCallbackRequest request)
+		{
+			return BadRequest();
+		}
+	}
+}
