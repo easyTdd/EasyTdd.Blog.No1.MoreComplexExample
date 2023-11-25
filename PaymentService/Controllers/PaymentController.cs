132b411b9e7e2a9f1bfdc0818e4ec672c6406575
kazys <kazys.rackauskas@gmail.com>
2023-11-25 21:10:11 +0200

    Refactoring

--- PaymentService/Controllers/PaymentController.cs
+++ PaymentService/Controllers/PaymentController.cs
@@ -10,22 +10,32 @@ namespace EasyTdd.Blog.No1.MoreComplexExample.PaymentService.Controllers
 		[HttpPost("callback")]
 		public IActionResult Callback(PaymentCallbackRequest request)
 		{
-			if (request.AmountPaid < 0)
-			{
-				return BadRequest();
-			}
-
-			if (string.IsNullOrWhiteSpace(request.InvoiceNo))
-			{
-				return BadRequest();
-			}
-
-			if (string.IsNullOrWhiteSpace(request.PaymentReference))
+			if (!IsRequestValid(request))
 			{
 				return BadRequest();
 			}
 
 			throw new NotImplementedException();
+		}
+
+		private static bool IsRequestValid(PaymentCallbackRequest request)
+		{
+			if (request.AmountPaid < 0)
+			{
+				return false;
+			}
+
+			if (string.IsNullOrWhiteSpace(request.InvoiceNo))
+			{
+				return false;
+			}
+
+			if (string.IsNullOrWhiteSpace(request.PaymentReference))
+			{
+				return false;
+			}
+
+			return true;
 		}
 	}
 }
