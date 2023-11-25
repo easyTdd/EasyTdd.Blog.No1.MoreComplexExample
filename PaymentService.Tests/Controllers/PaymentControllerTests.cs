24f60a2ca85bae975d4371b484cfc30ce268cd6b
kazys <kazys.rackauskas@gmail.com>
2023-11-25 21:06:47 +0200

    PaymentControllerTests with boundary tests

---
+++ PaymentService.Tests/Controllers/PaymentControllerTests.cs
@@ -0,0 +1,83 @@
+﻿using EasyTdd.Blog.No1.MoreComplexExample.PaymentService.Controllers;
+using EasyTdd.Blog.No1.MoreComplexExample.PaymentService.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace EasyTdd.Blog.No1.MoreComplexExample.PaymentService.Tests.Controllers
+{
+	public class PaymentControllerTests
+	{
+		private PaymentCallbackRequest _request;
+
+		[SetUp]
+		public void Setup()
+		{
+			_request = default;
+		}
+
+		[TestCase(null)]
+		[TestCaseSource(nameof(GetCallbackThrowsArgumentExceptionWhenRequestIsNotValidCases))]
+		public void CallbackThrowsArgumentExceptionWhenRequestIsNotValid(
+			PaymentCallbackRequest request)
+		{
+			_request = request;
+
+			var result = CallCallback();
+
+			result
+				.Should()
+				.BeOfType<BadRequestResult>();
+		}
+
+		private static IEnumerable<TestCaseData> GetCallbackThrowsArgumentExceptionWhenRequestIsNotValidCases()
+		{
+			yield return new TestCaseData(
+					new PaymentCallbackRequest
+					{
+						PaymentReference = "xx",
+						InvoiceNo = "yy",
+						AmountPaid = -1
+					}
+				)
+				.SetName("Amount is negative");
+
+			yield return new TestCaseData(
+					new PaymentCallbackRequest
+					{
+						PaymentReference = "xx",
+						InvoiceNo = "",
+						AmountPaid = 10
+					}
+				)
+				.SetName("InvoiceNo is not set");
+
+			yield return new TestCaseData(
+					new PaymentCallbackRequest
+					{
+						PaymentReference = "",
+						InvoiceNo = "yy",
+						AmountPaid = 10
+					}
+				)
+				.SetName("PaymentReference is not set");
+		}
+
+		private IActionResult CallCallback()
+		{
+			var sut = Create();
+
+			return sut
+				.Callback(
+					_request
+				);
+		}
+
+		private PaymentController Create()
+		{
+			return new PaymentController();
+		}
+	}
+}
